be26ef8c78f7da214f1cfcf1bdc5ab33626ed068
MateuszK <mateuszkulawiak98@gmail.com>
2020-12-09 23:26:05 +0100

    Adding changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,24 @@ const server = 'http://localhost:3000'
 const feed = `${server}/update/${process.platform}/${app.getVersion()}`
 
 autoUpdater.setFeedURL(feed)
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
 
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
 setInterval(() => {
   autoUpdater.checkForUpdates()
 }, 60000)
